3a16db148c5ee17b6821577c9690dbb249faa42e
Jake <jdeekhof@gmail.com>
2022-04-09 15:44:25 -0400

    split string problem and solution

--- problem_set/split_string/split_string.js
+++ problem_set/split_string/split_string.js
@@ -1,1 +1,26 @@
 split_string.js
+
+####first idea
+function solution(str){
+  let arr = []
+  for (n in str){
+    let x = str[n]
+    try {const y = str[n+1]}
+    catch (error) {const y = "_"}
+    arr.concat([x,y].join(''))
+  }
+  return(arr)
+}
+
+####second idea
+
+function solution(str){
+ let arr = []
+ if(str.length % 2){
+   str = str + "_"
+ }
+  for (let i = 0; i < str.length ; i += 2){
+    arr.push(str.slice(i,i+2))
+  }
+  return arr
+}
